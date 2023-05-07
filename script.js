3cf3e5a2da37bf304bb12cf08c64ef0558e2b9a0
mbfakourii <mbfakourii@gmail.com>
2023-05-07 23:26:04 +0430

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('#translate-btn').click(function() {
+        var inputText = $('#input').val();
+        var sourceLang = $('#source-language').val();
+        var targetLang = $('#target-language').val();
+		
+        $.ajax({
+            url: 'https://api.openai.com/v1/chat/completions',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer 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'
+            },
+            method: 'POST',
+            data: JSON.stringify({
+                'messages': [{"role": "user", "content":  `Translate "${inputText}" to ${targetLang} from ${sourceLang}`,}],
+				"model": "gpt-3.5-turbo",
+                'max_tokens': 1024,
+                'temperature': 0.5,
+                'n': 1,
+            }),
+            success: function(response) {
+                var outputText = response.choices[0].message.content;
+				outputText=outputText.replace(/"/g, '');
+                $('#output').val(outputText);
+            },
+            error: function() {
+                alert('Error in sending request to server. Please wait for 20 seconds and then submit the request.');
+            }
+        });
+    });
+});
